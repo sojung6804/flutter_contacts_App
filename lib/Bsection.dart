952a0a4e170137e69cf2230d84c52c04f1b13395
sopy <gorhtlfj2835@gmail.com>
2022-01-25 13:12:01 +0900

    2. 탭나누기, 레이아웃

---
+++ lib/Bsection.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Bsection extends StatelessWidget {
+  const Bsection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('B섹션'),
+    );
+  }
+}
